e418be2bce5cf24fe6f5f648834203d247ccf3fe
mguntay <guntay11@gmail.com>
2019-10-26 13:55:38 +0300

    Add project files.

---
+++ degiskenler/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace degiskenler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           byte s1=255;
+
+        }
+    }
+}
